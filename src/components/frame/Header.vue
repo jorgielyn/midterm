1ce6f25b55247fd18edd454bc6bd04b91ba23e13
jorgielyn <irangabriellef14@gmail.com>
2019-10-08 10:59:20 +0800

    suicidal

---
+++ src/components/frame/Header.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Iran</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+        <b-nav-item href="#">Login</b-nav-item>
+        <b-nav-item href="#" disabled>Register</b-nav-item>
+  </b-navbar>
+</div> 
+</template>
